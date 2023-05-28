43d951eeb66208bbb281c751170b2e355f570fdf
IELA <andreitumbaga29@gmail.com>
2023-05-28 22:03:58 +0800

    finishline

--- PESTE/Assets/Scripts/FinishLine.cs
+++ PESTE/Assets/Scripts/FinishLine.cs
@@ -6,23 +6,27 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     public GameManagerScript gameManager;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerEnter2D(Collider2D collision) 
-    { 
-        if(collision.tag == "Player")
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
         {
-            gameManager.gameOver();
+            Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+            playerRigidbody.velocity = Vector2.zero;
+            playerRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+            gameManager.victory();
         }
     }
 }
